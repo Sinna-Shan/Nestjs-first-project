91de06160e4dd07d7df851d3a96222482e6f03f8
Noah Shan <lust595@gmail.com>
2024-03-21 19:02:02 +0530

    global exception handling

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './all-exseptions.filter';
+// import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
+
+  //====To use logger globally====
+  // const app = await NestFactory.create(AppModule, {
+  //   bufferLogs: true,
+  // });
+  // app.useLogger(app.get(MyLoggerService));
   app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(3000);
